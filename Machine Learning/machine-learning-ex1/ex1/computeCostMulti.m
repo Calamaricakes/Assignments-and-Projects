fa17fce8d01433ba1ec9d125153649156b31c7a6
Nge Nern Lai <nlai1@student.unimelb.edu.au>
2019-03-29 15:55:03 +1100

    finalised first assignment of Machine Learning

--- Machine Learning/machine-learning-ex1/ex1/computeCostMulti.m
+++ Machine Learning/machine-learning-ex1/ex1/computeCostMulti.m
@@ -6,16 +6,24 @@ function J = computeCostMulti(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+% computes the cost function, J(theta).
+% (X * theta) -> matrix of values produced by the hypothesis
+% (X * theta - y).^2 ) -> a matrix containing the Squared Error values
+% sum ( (X * theta - y).^2 ) -> add up the total errors produced by hypothesis
+% sum ( (X * theta - y).^2 ) / (2 * m) -> divides the total by the number of
+% training examples
 
 
+% Vectorised form of J = sum ( (X * theta - y).^2 ) / (2 * m);
 
+J = (X * theta - y)' * (X * theta - y) / (2 * m);
 
 % =========================================================================
 
--- Machine Learning/machine-learning-ex1/ex1/ex1_multi.m
+++ Machine Learning/machine-learning-ex1/ex1/ex1_multi.m
@@ -3,11 +3,11 @@
 %
 %  Instructions
 %  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear regression exercise. 
 %
-%  You will need to complete the following functions in this 
+%  This file contains code that helps you get started on the
+%  linear regression exercise.
+%
+%  You will need to complete the following functions in this
 %  exericse:
 %
 %     warmUpExercise.m
@@ -60,13 +60,13 @@ X = [ones(m, 1) X];
 % ====================== YOUR CODE HERE ======================
 % Instructions: We have provided you with the following starter
 %               code that runs gradient descent with a particular
-%               learning rate (alpha). 
+%               learning rate (alpha).
 %
-%               Your task is to first make sure that your functions - 
-%               computeCost and gradientDescent already work with 
+%               Your task is to first make sure that your functions -
+%               computeCost and gradientDescent already work with
 %               this starter code and support multiple variables.
 %
-%               After that, try running gradient descent with 
+%               After that, try running gradient descent with
 %               different values of alpha and see which one gives
 %               you the best result.
 %
@@ -82,16 +82,18 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
+alpha = 1.00;
 num_iters = 400;
 
-% Init Theta and Run Gradient Descent 
+% Init Theta and Run Gradient Descent
 theta = zeros(3, 1);
+
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
 % Plot the convergence graph
 figure;
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+hold on;
 xlabel('Number of iterations');
 ylabel('Cost J');
 
@@ -104,7 +106,11 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+house = [1650 3];
+house_norm = (house - mu) ./ sigma;
+house_norm = [1 house_norm];
+price = house_norm * theta;
+
 
 
 % ============================================================
@@ -120,12 +126,12 @@ pause;
 fprintf('Solving with normal equations...\n');
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: The following code computes the closed form 
+% Instructions: The following code computes the closed form
 %               solution for linear regression using the normal
-%               equations. You should complete the code in 
+%               equations. You should complete the code in
 %               normalEqn.m
 %
-%               After doing so, you should complete this code 
+%               After doing so, you should complete this code
 %               to predict the price of a 1650 sq-ft, 3 br house.
 %
 
@@ -149,11 +155,10 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1 house] * theta; % You should change this
 
 
 % ============================================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using normal equations):\n $%f\n'], price);
-
