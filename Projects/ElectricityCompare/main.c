c59746a2d3c5a1417ae1c184cea2c8db56eef554
Nge Nern Lai <laingenern@gmail.com>
2019-07-08 17:13:23 +1000

    continuing with ElectricityCompare

--- Projects/ElectricityCompare/main.c
+++ Projects/ElectricityCompare/main.c
@@ -1,14 +1,43 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char** argv){
-    int number1, number2;
+#define LINE_INPUT_SIZE 1024
+#define SMART_METER_READER_CSV "C:\\Users\\Prothean\\Desktop\\Code Folder\\Projects\\ElectricityCompare\\Electricity\\Victorian Energy Compare Data.csv"
 
-    printf("Enter a number:");
-    scanf("%d", &number1);
-    printf("Number is %d\n", number1);
-    printf("Enter a number:");
-    scanf("%d", &number2);
-    printf("Number is %d\n", number2);
+int main(){
+
+    // File handing variables
+    FILE* fp = NULL;
+
+    char consumptionDataPerDay[LINE_INPUT_SIZE];
+    char titleLine[LINE_INPUT_SIZE];
+
+    char* token_ptr;
+
+    fp = fopen(SMART_METER_READER_CSV, "r");
+
+    if(fp != NULL){
+        printf("TITLE LINE\n");
+        fgets(titleLine, LINE_INPUT_SIZE, fp);
+        printf("%s\n", titleLine);
+
+        // get one line input
+        fgets(consumptionDataPerDay, LINE_INPUT_SIZE, fp);
+        printf("%s\n", consumptionDataPerDay);
+
+        printf("START TOKENIZING\n");
+        token_ptr = strtok(titleLine,",");
+
+        while(token_ptr){
+            printf("%s\n", token_ptr);
+            token_ptr = strtok(NULL, ",");
+        }
+
+    }
+    else{
+        printf("File not found\n");
+        return 0;
+    }
 
     return 0;
 }
