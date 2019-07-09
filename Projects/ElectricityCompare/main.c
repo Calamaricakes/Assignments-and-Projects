bc53b995039ea3a1bb04ccdf21f382d985da67bc
Nge Nern Lai <nlai1@student.unimelb.edu.au>
2019-07-09 15:54:27 +1000

    added some data nodes

--- Projects/ElectricityCompare/main.c
+++ Projects/ElectricityCompare/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
+#define TRUE 1
+#define FALSE 0
 #define LINE_INPUT_SIZE 1024
-#define SMART_METER_READER_CSV "C:\\Users\\Prothean\\Desktop\\Code Folder\\Projects\\ElectricityCompare\\Electricity\\Victorian Energy Compare Data.csv"
+#define HALF_HOURS_IN_A_DAY 48
+#define SMART_METER_READER_CSV_Protean "C:\\Users\\Prothean\\Desktop\\Code Folder\\Projects\\ElectricityCompare\\Electricity\\Victorian Energy Compare Data.csv"
+#define SMART_METER_READER_CSV_USER "C:\\Users\\User\\Dropbox\\Electricity\\Victorian Energy Compare Data.csv"
+
+typedef struct DateNode{
+    int day;
+    int month;
+    int year;
+}DateNode;
+
+typedef struct DayUsageNode{
+    int NMI_nationalMeterIdentifier;
+    int meterSerialNumber;
+    int consumingElectricity;
+    int generatingElectricity;
+    DateNode* date_ptr;
+    float consumptionByHalfHour[HALF_HOURS_IN_A_DAY];
+}DayUsageNode;
+
 
 int main(){
 
@@ -14,19 +34,21 @@ int main(){
 
     char* token_ptr;
 
-    fp = fopen(SMART_METER_READER_CSV, "r");
+    fp = fopen(SMART_METER_READER_CSV_USER, "r");
 
     if(fp != NULL){
         printf("TITLE LINE\n");
         fgets(titleLine, LINE_INPUT_SIZE, fp);
         printf("%s\n", titleLine);
 
-        // get one line input
+        // get a line that corresponds to the
+        // electricity usage of that day
+        //while
         fgets(consumptionDataPerDay, LINE_INPUT_SIZE, fp);
         printf("%s\n", consumptionDataPerDay);
 
         printf("START TOKENIZING\n");
-        token_ptr = strtok(titleLine,",");
+        token_ptr = strtok(consumptionDataPerDay,",");
 
         while(token_ptr){
             printf("%s\n", token_ptr);
