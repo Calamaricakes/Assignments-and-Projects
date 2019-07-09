1de8daba2d87aca77501a48f5a3a5c70aa86c247
Nge Nern Lai <nlai1@student.unimelb.edu.au>
2019-07-09 17:03:01 +1000

    progress on ElectricityCompare

--- Projects/ElectricityCompare/main.c
+++ Projects/ElectricityCompare/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <malloc.h>
 
 #define TRUE 1
 #define FALSE 0
+#define EQUAL 0
 #define LINE_INPUT_SIZE 1024
 #define HALF_HOURS_IN_A_DAY 48
+#define NMI_LENGTH 11
+#define SERIAL_NUMBER_LENGTH 6
 #define SMART_METER_READER_CSV_Protean "C:\\Users\\Prothean\\Desktop\\Code Folder\\Projects\\ElectricityCompare\\Electricity\\Victorian Energy Compare Data.csv"
 #define SMART_METER_READER_CSV_USER "C:\\Users\\User\\Dropbox\\Electricity\\Victorian Energy Compare Data.csv"
 
@@ -15,45 +20,39 @@ typedef struct DateNode{
 }DateNode;
 
 typedef struct DayUsageNode{
-    int NMI_nationalMeterIdentifier;
-    int meterSerialNumber;
-    int consumingElectricity;
+    char[NMI_LENGTH] NMI_nationalMeterIdentifier;
+    char[SERIAL_NUMBER_LENGTH] meterSerialNumber;
     int generatingElectricity;
     DateNode* date_ptr;
     float consumptionByHalfHour[HALF_HOURS_IN_A_DAY];
 }DayUsageNode;
 
+DayUsageNode* processElectricityData(char* stringConsumptionData);
 
 int main(){
 
     // File handing variables
-    FILE* fp = NULL;
+    FILE* file_ptr = NULL;
 
     char consumptionDataPerDay[LINE_INPUT_SIZE];
     char titleLine[LINE_INPUT_SIZE];
 
-    char* token_ptr;
+    char* token_ptr = NULL;
 
-    fp = fopen(SMART_METER_READER_CSV_USER, "r");
+    file_ptr = fopen(SMART_METER_READER_CSV_USER, "r");
 
-    if(fp != NULL){
+    if(file_ptr != NULL){
         printf("TITLE LINE\n");
-        fgets(titleLine, LINE_INPUT_SIZE, fp);
+        fgets(titleLine, LINE_INPUT_SIZE, file_ptr);
         printf("%s\n", titleLine);
 
         // get a line that corresponds to the
         // electricity usage of that day
         //while
-        fgets(consumptionDataPerDay, LINE_INPUT_SIZE, fp);
+        fgets(consumptionDataPerDay, LINE_INPUT_SIZE, file_ptr);
         printf("%s\n", consumptionDataPerDay);
 
-        printf("START TOKENIZING\n");
-        token_ptr = strtok(consumptionDataPerDay,",");
-
-        while(token_ptr){
-            printf("%s\n", token_ptr);
-            token_ptr = strtok(NULL, ",");
-        }
+        processElectricityData(consumptionDataPerDay);
 
     }
     else{
@@ -62,4 +61,54 @@ int main(){
     }
 
     return 0;
+}
+
+DayUsageNode* processElectricityData(char* stringConsumptionData){
+// interprets a line of csv data then puts the processed
+// information into DayUsageNode and DateNode
+
+    char* token_ptr = NULL;
+    DayUsageNode* usageNode_ptr = (DayUsageNode*)malloc(1);
+
+    // initialise the tokeniser
+    token_ptr = strtok(stringConsumptionData,",");
+
+    // start reading each section of string separated by ","
+    usageNode_ptr->NMI_nationalMeterIdentifier = atoi(token_ptr);
+    token_ptr = strtok(NULL, ",");
+
+    usageNode_ptr->meterSerialNumber = atoi(token_ptr);
+    token_ptr = strtok(NULL, ",");
+
+    // if the household is consuming electricity
+    if(strcmp(token_ptr, "Consumption") == EQUAL){
+        usageNode_ptr->generatingElectricity = FALSE;
+    }
+    // else it is generating electricty
+    else{
+        usageNode_ptr->generatingElectricity = TRUE;
+    }
+    token_ptr = strtok(NULL, ",");
+
+    // it gets a little messy here, the dates are in a dd/mm/yyyy format
+    // so we have to tokenise the tokenised string again
+
+    char* token_date_ptr = NULL;
+    DateNode* dateNode_ptr = (DateNode*)malloc(1);
+
+    token_date_ptr = strtok(token_ptr, "/");
+    dateNode_ptr->day = atoi(token_date_ptr);
+
+    token_date_ptr = strtok(NULL, "/");
+    dateNode_ptr->month = atoi(token_date_ptr);
+
+    token_date_ptr = strtok(NULL, "/");
+    dateNode_ptr->year = atoi(token_date_ptr);
+
+    token_date_ptr = strtok(NULL, "/"); // NULL the ptr
+
+    //
+    // precaution
+    free(usageNode_ptr);
+    free(dateNode_ptr);
 }
