c2f3f8424bf38ae7ed4b4b711d83d0a72b4461af
Nge Nern Lai <nlai1@student.unimelb.edu.au>
2019-09-18 13:32:23 +1000

    Django tutorial finished

---
+++ Projects/TutorialWebsite/PortfolioSite-Project/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\coding~1\assign~1\projects\tutori~1\portfo~1\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
